17a7682c52b213c8223024a1beae4b89936d548c
Andrew Grosner <andrew.grosner@raizlabs.com>
2015-05-17 14:59:05 -0400

    added tests to verify count + positioning

--- UniversalAdapter/src/androidTest/java/com/raizlabs/universaladapter/test/merged/MergedAdapterTest.java
+++ UniversalAdapter/src/androidTest/java/com/raizlabs/universaladapter/test/merged/MergedAdapterTest.java
@@ -81,10 +81,77 @@ public class MergedAdapterTest extends UniversalAdapterTestCase {
             }
         });
 
+        // ensure adding an adapter triggers proper notification (even if empty)
         MergedAdapter1 adapter1 = new MergedAdapter1();
         mergedUniversalAdapter.addAdapter(adapter1);
         assertTrue(called[1]);
         assertTrue(starts[1] == 0);
         assertTrue(counts[1] == 0);
+        called[1] = false;
+
+        // ensure add first item works as expected
+        adapter1.add("Test");
+        assertTrue(called[1]);
+        assertTrue(starts[1] == 0);
+        assertTrue(counts[1] == 1);
+        called[1] = false;
+
+        // ensure second adapter adds where its supposed to.
+        MergedAdapter1 adapter2 = new MergedAdapter1();
+        mergedUniversalAdapter.addAdapter(adapter2);
+        assertTrue(called[1]);
+        assertTrue(starts[1] == 1);
+        assertTrue(counts[1] == 0);
+        called[1] = false;
+
+        // ensure second adapter add propagates properly to merged listener
+        adapter2.add("Test");
+        assertTrue(called[1]);
+        assertTrue(starts[1] == 1);
+        assertTrue(counts[1] == 1);
+        called[1] = false;
+
+        // ensure we can "splice" an item between first and second adapter
+        adapter1.add("Test");
+        assertTrue(called[1]);
+        assertTrue(starts[1] == 1);
+        assertTrue(counts[1] == 1);
+        called[1] = false;
+
+        // ensure we can remove an item between as well
+        adapter1.remove(1);
+        assertTrue(called[2]);
+        assertTrue(starts[1] == 1);
+        assertTrue(counts[1] == 1);
+        called[2] = false;
+
+        // test prepopulated adapter inserted
+        MergedAdapter1 adapter3 = new MergedAdapter1();
+        adapter3.add("Test");
+        adapter3.add("Another");
+        mergedUniversalAdapter.addAdapter(adapter3);
+        assertTrue(called[1]);
+        assertTrue(starts[1] == 2);
+        assertTrue(counts[1] == 2);
+        called[1] = false;
+
+        // test all 3 adapters to ensure changing data also changes correctly
+        adapter1.set(0, "Test2");
+        assertTrue(called[0]);
+        assertTrue(starts[0] == 0);
+        assertTrue(counts[0] == 1);
+        called[0] = false;
+
+        adapter2.set(0, "Test2");
+        assertTrue(called[0]);
+        assertTrue(starts[0] == 1);
+        assertTrue(counts[0] == 1);
+        called[0] = false;
+
+        adapter3.set(0, "Test2");
+        assertTrue(called[0]);
+        assertTrue(starts[0] == 2);
+        assertTrue(counts[0] == 1);
+        called[0] = false;
     }
 }
