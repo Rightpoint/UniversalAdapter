78c0eb1600631a4889ce5a98482c1e2553cec28a
Dylan James <dylanrjames@gmail.com>
2015-10-27 12:21:50 -0400

    Added getContext() to ViewHolder

--- UniversalAdapter/src/main/java/com/raizlabs/universaladapter/ViewHolder.java
+++ UniversalAdapter/src/main/java/com/raizlabs/universaladapter/ViewHolder.java
@@ -1,5 +1,6 @@
 package com.raizlabs.universaladapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -7,5 +8,17 @@ public class ViewHolder extends RecyclerView.ViewHolder {
 
     public ViewHolder(View itemView) {
         super(itemView);
+    }
+
+    /**
+     * Returns the {@link Context} of this {@link ViewHolder}, based on its {@link #itemView}.
+     * @return The context from the item view, or null if there is no item view.
+     */
+    public Context getContext() {
+        if (itemView != null) {
+            return itemView.getContext();
+        } else {
+            return null;
+        }
     }
 }
